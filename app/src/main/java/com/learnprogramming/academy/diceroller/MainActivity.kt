9330bdfe6033b660234dda43fb054818681ce234
Mubanga <mubangeriseme@yahoo.com>
2019-07-08 02:34:57 +0200

    Lesson 1 Activity 16---> Dynamically Modify Button View

--- app/src/main/java/com/learnprogramming/academy/diceroller/MainActivity.kt
+++ app/src/main/java/com/learnprogramming/academy/diceroller/MainActivity.kt
@@ -2,11 +2,15 @@ package com.learnprogramming.academy.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private val _btnRoll by lazy { findViewById<Button>(R.id.btn_Roll) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        _btnRoll.text = "Let's Roll"
     }
 }
