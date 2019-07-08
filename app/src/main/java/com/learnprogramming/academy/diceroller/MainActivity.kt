c274b5e6a9a034b44ec934362c46c6f7a5e3effb
Mubanga <mubangeriseme@yahoo.com>
2019-07-08 02:46:35 +0200

    Lesson 1 Activity 18 ---> Random() numbers generated in for Dice Roll

--- app/src/main/java/com/learnprogramming/academy/diceroller/MainActivity.kt
+++ app/src/main/java/com/learnprogramming/academy/diceroller/MainActivity.kt
@@ -3,18 +3,29 @@ package com.learnprogramming.academy.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private val _btnRoll by lazy { findViewById<Button>(R.id.btn_Roll) }
+    private val _txtResult by lazy { findViewById<TextView>(R.id.txtView_DiceRoll)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         _btnRoll.text = "Let's Roll"
         _btnRoll.setOnClickListener {
-            Toast.makeText(this,"Button Has Been Clicked",Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+
+
+        val randomNumber = Random().nextInt(6) + 1
+        _txtResult.text = randomNumber.toString()
+
     }
 }
