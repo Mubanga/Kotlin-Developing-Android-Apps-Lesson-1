85a6978232e63eda3052b834816e5fa94655a33c
Mubanga <mubangeriseme@yahoo.com>
2019-07-08 03:05:26 +0200

    Lesson 1 Activity 21 ---> Exercise: Adding the ImageView

--- app/src/main/java/com/learnprogramming/academy/diceroller/MainActivity.kt
+++ app/src/main/java/com/learnprogramming/academy/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.learnprogramming.academy.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -10,12 +11,13 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     private val _btnRoll by lazy { findViewById<Button>(R.id.btn_Roll) }
-    private val _txtResult by lazy { findViewById<TextView>(R.id.txtView_DiceRoll)}
+    private val _imgDiceRoll by lazy { findViewById<ImageView>(R.id.img_DiceRoll) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         _btnRoll.text = "Let's Roll"
+        _imgDiceRoll.setImageResource(R.drawable.dice_2)
         _btnRoll.setOnClickListener {
             rollDice()
         }
@@ -24,8 +26,15 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
 
 
-        val randomNumber = Random().nextInt(6) + 1
-        _txtResult.text = randomNumber.toString()
+        val drawableDiceImage = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        _imgDiceRoll.setImageResource(drawableDiceImage)
 
     }
 }
